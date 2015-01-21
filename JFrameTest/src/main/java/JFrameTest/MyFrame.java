ee7a1e1a4bce417209d2c648e0361caa9ebb2048
krat <krat@ufins.com>
2015-01-21 13:12:11 +0200

    JFrame Test

--- JFrameTest/src/main/java/JFrameTest/MyFrame.java
+++ JFrameTest/src/main/java/JFrameTest/MyFrame.java
@@ -4,13 +4,18 @@ package JFrameTest;
 //Импортируем нужные пространства имен.
 import java.awt.*;
 import java.awt.event.*;
-
 import javax.swing.*;
+import javax.swing.plaf.basic.*;
+
+import org.w3c.dom.Text;
+
+
 //Класс основного окна программы.
 public class MyFrame extends JFrame {
 
 	JTextField testName, jTextField1;
-
+	private Text       textBox;
+	
 	// Переменная для кнопки.
 	public JButton button;
 	// Конструктор.
@@ -20,34 +25,61 @@ public class MyFrame extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		// Устанавливаем размеры и расположение.
 		setLocation(750, 350);
-		setSize(300, 500);
+		setSize(500, 500);
 		setTitle("Title");
 		// Задаем контейнер для компонентов.
-		Container con = getContentPane();
-		con.setLayout(new FlowLayout());
+//		Container con = getContentPane();
+//		con.setLayout(new FlowLayout());
+		JPanel panel = new JPanel();
+	    panel.setLayout(null);
 
-		// JLabel
-		JLabel label = new JLabel("test name");
-		con.add(label);
+
+	    // textArea
+	    String textAreaString = "<?xml version='1.0' encoding='UTF-8'?>\n"
+	    +"<!DOCTYPE suite SYSTEM 'http://testng.org/testng-1.0.dtd'>\n"
+	    +"<suite name='Search Chek Web' parallel='tests' thread-count='1'>";
+	    TextArea textAreaDeader = new TextArea(textAreaString);
+	    textAreaDeader.setBounds(10, 10, 400, 75);
+		panel.add(textAreaDeader);
+	    
+	    TextArea textAreaBody = new TextArea(textAreaString);
+	    textAreaBody.setBounds(10, 100, 400, 75);
+		panel.add(textAreaBody);
 		
+		
+/*	
 		// Создание JTextField
-		testName = new JTextField("test name example:", 20);
+		testName = new JTextField("test name example:");
+		testName.setBounds(10, 30, 300, 100);
 		// Добавление JTextField к контейнеру.
-		con.add(testName);
+		panel.add(testName);
+		
+		
 
 		// JLabel
 		JLabel label1 = new JLabel("Test1");
-		con.add(label1);
+		label1.setBounds(10, 150, 100, 10);
+		panel.add(label1);
 		
+
 		// Создание JTextField
 		jTextField1 = new JTextField("example1:", 20);
+		jTextField1.setBounds(10, 200, 300, 100);
 		// Добавление JTextField к контейнеру.
-		con.add(jTextField1);
+		panel.add(jTextField1);
 
+		
+		
+		textArea = new TextArea("Ваш заказ:");
+		textArea.setBounds(10, 300, 300, 100);
+		panel.add(textArea);
+*/
+		
 		// Создание кнопки.
 		button = new JButton("Press me");
+		button.setBounds(10, 400, 100, 25);
 		// Добавление кнопки к контейнеру.
-		con.add(button);
+		panel.add(button);
 
 		// Добавление обработчика для кнопки.
 		button.addActionListener(new ActionListener(){
@@ -70,6 +102,10 @@ public class MyFrame extends JFrame {
 			}
 		});
 
+		
+        getContentPane().add(panel);
+        setPreferredSize(new Dimension(285, 145));
+        
 	}
 
 	public static void main(String[] args) {
